7507b3c6b2962419919b933777a831f0b67dea9c
Edwin <Edwinallen94@aol.com>
2024-10-06 23:00:46 -0400

    final comments and touch ups

--- script/frontEnd.js
+++ script/frontEnd.js
@@ -1,4 +1,6 @@
+// Function to show off the random cat pic on the page
+
 export function displayCat(catUrl) {
-  const container = document.getElementById("cat-container");
-  container.innerHTML = `<img src="${catUrl}" alt="Random Cat">`;
+  const container = document.getElementById("cat-container"); // Finding the spot to drop the cat
+  container.innerHTML = `<img src="${catUrl}" alt="Random Cat">`; // Displaying that cat, loud and clear
 }
