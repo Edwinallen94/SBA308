e2e336feb5a379beca87638af8ca73f1f3302fde
Edwin <Edwinallen94@aol.com>
2024-10-06 22:57:13 -0400

    adding comments to each of my section and consolidating

--- script/cats.js
+++ script/cats.js
@@ -1,9 +1,15 @@
+//// Function to fetch that random cat pic from the API
 export async function fetchRandomCat() {
   try {
+    // fetching from the cat api eithout delays
     const response = await fetch("https://api.thecatapi.com/v1/images/search");
+    //catching the data
     const data = await response.json();
+
+    //finally passing back the cat pic url
     return data[0].url;
   } catch (error) {
+    //catching the erros
     console.error("Error fetching cat data:", error);
   }
 }
--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
+//migrating the functions from the other files.
+
 import { fetchRandomCat } from "./cats.js";
 import { displayCat } from "./frontEnd.js";
 
+//making sure the click button works
 document.getElementById("new-cat").addEventListener("click", async () => {
   const catData = await fetchRandomCat();
   displayCat(catData);
